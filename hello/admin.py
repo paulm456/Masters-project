b1f2efe7f3c1a98234a7d67e7a4293dd1563102c
Paul <paul.mulpeter2@mail.dcu.ie>
2026-01-18 22:46:02 +0000

    Add CSV export action to SensorReading admin

--- hello/admin.py
+++ hello/admin.py
@@ -1,8 +1,39 @@
+import csv
+from django.http import HttpResponse
 from django.contrib import admin
 from .models import SensorReading
 
 # Register your models here.
 @admin.register(SensorReading)
 class SensorReadingAdmin(admin.ModelAdmin):
-    list_display = ('pm1', 'pm25', 'pm10', 'timestamp')
+    list_display = ('timestamp', 'pm1', 'pm25', 'pm10')
+    list_filter = ('timestamp',)
     ordering = ('-timestamp',)
+    actions = [export_as_csv]
+
+
+def export_as_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=sensor_readings.csv'
+
+    writer = csv.writer(response)
+    writer.writerow([
+        'Timestamp',
+        'PM1',
+        'PM2.5',
+        'PM10',
+    ])
+
+    for reading in queryset:
+        writer.writerow([
+            reading.timestamp,
+            reading.pm1,
+            reading.pm25,
+            reading.pm10
+        ])
+
+
+    return response
+
+
+export_as_csv.short_description = "Export selected rows as CSV"
